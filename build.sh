31439b92171f53720ddf4d4d08c3b0d0aafef526
Shawon Sheikh <sxs1748@cs.bham.ac.uk>
2022-05-12 03:10:18 +0100

    build

--- build.sh
+++ build.sh
@@ -1,10 +1,7 @@
 #exit on error
 set -o errexit
-python -m pip install -r requirements.txt
-ls env/Scripts
-uname -r
-source env/Scripts/activate
-python manage.py collectstatic --no-input
-python manage.py migrate
+python3 --version
+python --version
+python3 manage.py migrate
 
 
